210215cf5f3d8cd1fe6f2083f57c37f37d2ea8d1
Camila Fabbroni <camila.fabbroni1985@gmail.com>
2023-10-20 21:03:24 -0300

    formateo en app2.js

--- app2.js
+++ app2.js
@@ -1,16 +1,21 @@
+// Listas de pronombres, adjetivos y sustantivos
 let pronombres = ['tu', 'nuestro', 'mi'];
 let adjetivos = ['gran', 'primer', 'mejor'];
 let sustantivos = ['dominio', 'sitio', 'empresa'];
 
+// Función para generar todas las combinaciones posibles de nombres de dominio
 function generarCombinaciones() {
-  let combinaciones = pronombres.flatMap(pronombre =>
-    adjetivos.flatMap(adjetivo =>
-      sustantivos.map(sustantivo => `${pronombre}${adjetivo}${sustantivo}.com`)
-    )
-  );
+    // Inicializar un array para almacenar las combinaciones
+    let combinaciones = pronombres.flatMap(pronombre =>
+        adjetivos.flatMap(adjetivo =>
+            sustantivos.map(sustantivo => `${pronombre}${adjetivo}${sustantivo}.com`)
+        )
+    );
 
-  return combinaciones;
+    // Devolver la lista de combinaciones de nombres de dominio
+    return combinaciones;
 }
 
+// Llamar a la función para generar combinaciones y almacenar el resultado en 'dominios'
 let dominios = generarCombinaciones();
-console.log(dominios);
+console.log(dominios);// Imprimir las combinaciones de nombres de dominio en la consola
